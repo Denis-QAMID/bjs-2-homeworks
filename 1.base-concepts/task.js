9af17a93ea5811b376409141bd29e3ef4b27a973
Denis Denisov <xeddyx@mail.ru>
2023-05-03 21:49:02 +0300

    update

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,17 +1,20 @@
 "use strict"
 function solveEquation(a, b, c) {
   const discriminant = b ** 2 - 4 * a * c;
+  let arr = [];
 
   if (discriminant < 0) {
-    return [];
+    // массив arr остается пустым
   } else if (discriminant === 0) {
     const root = -b / (2 * a);
-    return [root];
+    arr = [root];
   } else {
     const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
     const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-    return [root1, root2];
+    arr = [root1, root2];
   }
+
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
