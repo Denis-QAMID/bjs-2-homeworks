b1c11db6ab5270a20696be1c95ce641a48e9a780
Denis Denisov <xeddyx@mail.ru>
2023-05-03 22:21:36 +0300

    Initial Commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -18,5 +18,16 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (typeof percent !== "number" || typeof contribution !== "number" || typeof amount !== "number" || typeof countMonths !== "number") {
+    return false;
+  }
+
+  const monthlyPercent = percent / 100 / 12;
+  const creditBody = amount - contribution;
+
+  const monthlyPayment = creditBody * (monthlyPercent + monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) - 1));
+  const totalAmount = monthlyPayment * countMonths;
+
+  return +totalAmount.toFixed(2);
 }
+  
