b227389d5a72d3eb2a0c991a9fc8dd5ac304ed8d
Denis Denisov <xeddyx@mail.ru>
2023-05-19 22:51:40 +0300

    update3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,25 @@
+//Задача 1
+
 function compareArrays(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+      return false; 
+    }
   
-}
+    return arr1.every((value, index) => value === arr2[index]);
+  }
+
+//Задача 2
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let filteredUsers = users.filter(user => user.gender === gender);
+    
+    if (filteredUsers.length === 0) {
+      return 0; 
+    }
+    
+    let ages = filteredUsers.map(user => user.age);
+    let sum = ages.reduce((total, age) => total + age, 0);
+    let average = sum / ages.length;
+    
+    return average.toFixed(1);
 }
